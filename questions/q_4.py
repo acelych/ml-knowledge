07c35cbebfa10bf6f2cf5e626a472ce3ae7316f5
acelych <acelych@foxmail.com>
2025-09-12 21:01:37 +0300

    update: q4

--- questions/q_4.py
+++ questions/q_4.py
@@ -23,14 +23,144 @@
     - 输出M行预测结果。
 '''
 
+DEBUG_MODE = True
+
+if DEBUG_MODE:
+    import sys
+    import json
+    from io import StringIO
+    with open("questions/q_4.json", 'r') as f:
+        obj = json.load(f)
+        st = obj['main'][1]['i']
+        sys.stdin = StringIO('\n'.join(st))
 N = int(input())
 
 train_datas = []
 for _ in range(N):
-    train_datas.append(list(map(float, input().split())))
+    train_datas.append(input().split())
 
 M = int(input())
 
 predict_datas = []
 for _ in range(M):
     predict_datas.append(list(map(float, input().split())))
+
+# wash data
+it_sum, ot_sum, aid_sum, aod_sum, year_sum = [], [], [], [], []
+for data in train_datas:
+    data[1] = "NaN" if (data[1] == "NaN" or float(data[1]) < 0) else float(data[1])
+    data[2] = "NaN" if (data[2] == "NaN" or float(data[2]) < 0) else float(data[2])
+    data[3] = "NaN" if (data[3] == "NaN" or float(data[3]) < 0 or float(data[3]) > 1000) else float(data[3])
+    data[4] = "NaN" if (data[4] == "NaN" or float(data[4]) < 0 or float(data[4]) > 1000) else float(data[4])
+    data[5] = "NaN" if (data[5] == "NaN" or float(data[5]) < 0 or float(data[5]) > 20) else float(data[5])
+    data[6] = float(data[6])
+    if isinstance(data[1], float):
+        it_sum.append(data[1])
+    if isinstance(data[2], float):
+        ot_sum.append(data[2])
+    if isinstance(data[3], float):
+        aid_sum.append(data[3])
+    if isinstance(data[4], float):
+        aod_sum.append(data[4])
+    if isinstance(data[5], float):
+        year_sum.append(data[5])
+
+it = sum(it_sum)/len(it_sum)
+ot = sum(ot_sum)/len(ot_sum)
+aid = sum(aid_sum)/len(aid_sum)
+aod = sum(aod_sum)/len(aod_sum)
+year = sum(year_sum)/len(year_sum)
+    
+for i, data in enumerate(train_datas):
+    if isinstance(data[1], str):
+        data[1] = it
+    if isinstance(data[2], str):
+        data[2] = ot
+    if isinstance(data[3], str):
+        data[3] = aid
+    if isinstance(data[4], str):
+        data[4] = aod
+    if isinstance(data[5], str):
+        data[5] = year
+
+# normalize
+def normalize(datas: list, dims_max_min: list = None):
+    dims = len(datas[0])
+    if dims_max_min is None:
+        dims_max_min = []
+        record = True
+    else:
+        record = False
+    for dim in range(dims):
+        if record:
+            max_v = max([data[dim] for data in datas])
+            min_v = min([data[dim] for data in datas])
+            dims_max_min.append((max_v, min_v))
+        else:
+            max_v, min_v = dims_max_min[dim]
+        for data in datas:
+            data[dim] = (data[dim] - min_v) / (max_v - min_v)
+    return dims_max_min
+
+# logic regression
+import math
+
+weight = [0.0 for _ in range(len(train_datas[0]) - 2)]
+bias = 0.0
+learn_rate = 0.05
+
+def sigmoid(x: float):
+    return 1.0 / (1.0 + math.exp(-x))
+
+def diff_sigmoid(y: float):
+    return y * (1 - y)
+
+def diff_mse(pred: list, label: list) -> list:
+    return list([(p - l) * 2 / len(pred) for (p, l) in zip(pred, label)])
+
+def diff_bce_sigmoid(pred: list, label: list) -> list:
+    return list([y_p - y for (y_p, y) in zip(pred, label)])
+
+def forward_and_backward(x: list, label: list):
+    global weight, bias
+
+    # forward
+    y_mat = [sum([xn * wn for (xn, wn) in zip(x_batch, weight)]) + bias for x_batch in x]
+    y_sig = [sigmoid(yn) for yn in y_mat]
+    # loss
+    # grad_m = diff_mse(y_sig, label)
+    grad_m = [1.0 for _ in range(len(y_sig))]
+    # backward
+    # grad_s = [diff_sigmoid(yn) for yn in y_sig]
+    grad_s = diff_bce_sigmoid(y_sig, label)  # grad (bce multi sig)
+    grad_w = [[x[ib][iw] for ib in range(len(x))] for iw in range(len(weight))]
+    grad_b = 1
+    # step
+    grad_W = [sum([gw * gs * gm for (gw, gs, gm) in zip(gwi, grad_s, grad_m)])/len(x) for gwi in grad_w]
+    grad_B = sum([grad_b * gs * gm for (gs, gm) in zip(grad_s, grad_m)])/len(x)
+    weight = [w - learn_rate * gW for (w, gW) in zip(weight, grad_W)]
+    bias -= learn_rate * grad_B
+
+def forward(x: list):
+    y_mat = [sum([xn * wn for (xn, wn) in zip(x_batch, weight)]) + bias for x_batch in x]
+    y_sig = [sigmoid(yn) for yn in y_mat]
+    return [int(yn > 0.5) for yn in y_sig]
+
+# train
+epochs = 100
+x = [data[1:-1] for data in train_datas]
+label = [data[-1] for data in train_datas]
+dims_max_min = normalize(x)
+
+for epoch in range(epochs):
+    forward_and_backward(x, label)
+    pred = forward(x)
+    acc = sum([p == l for (p, l) in zip(pred, label)]) / len(label)
+    print(f"epoch: {epoch}, acc: {acc}")
+
+# predict
+x = [data[1:] for data in predict_datas]
+normalize(x, dims_max_min)
+
+pred = [str(p) for p in forward(x)]
+print(' '.join(pred))
